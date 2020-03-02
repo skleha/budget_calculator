47f169a2a7561a7175571e806d524d6a3655d854
thejenkdog <thejenkdog@gmail.com>
2020-03-02 12:27:51 -0800

    Move firestore initialize to constructor function in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React  from 'react';
 import "firebase/firestore";
 import './App.css';
+import * as firebase from 'firebase/app';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {isLoading: true}
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(
+        {
+          apiKey: "AIzaSyD7NUVfrImccSo8FuCBG7bXVk0oLFqgE-k",
+          authDomain: "yardzen-demo.firebaseapp.com",
+          databaseURL: "https://yardzen-demo.firebaseio.com",
+          projectId: "yardzen-demo",
+          storageBucket: "yardzen-demo.appspot.com",
+          messagingSenderId: "509183652730",
+          appId: "1:509183652730:web:ba2208f7d8e0882f009cc3"
+        }
+      )
+    }
+
+
   }
 
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase/app';
 import seeds from './seeds';
-
-firebase.initializeApp(
-   {
-    apiKey: "AIzaSyD7NUVfrImccSo8FuCBG7bXVk0oLFqgE-k",
-    authDomain: "yardzen-demo.firebaseapp.com",
-    databaseURL: "https://yardzen-demo.firebaseio.com",
-    projectId: "yardzen-demo",
-    storageBucket: "yardzen-demo.appspot.com",
-    messagingSenderId: "509183652730",
-    appId: "1:509183652730:web:ba2208f7d8e0882f009cc3"
-  }
-)
 
 window.seed = seeds;
 
